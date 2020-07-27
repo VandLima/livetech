ec1e81a7d1aaa9b216a4686b6ab93af8504e6546
Anadson  Passos <37884159+anadsonpassos@users.noreply.github.com>
2020-07-27 16:47:39 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -11,7 +11,7 @@ Before do |scenario|
     $store_id = ENV['store_id']
     $dunit = true
     $first = true
-    #$feature = scenario.feature.name
+    $feature = scenario.feature.name
     $app = App.new($store_id)
   end
     @mass_bkp = (YAML.load_file('./features/fixtures/mass.yaml'))
@@ -20,8 +20,8 @@ Before do |scenario|
     @env = $env
     $step_index = 0
     $scenario = scenario
-    #$scenario_path = "#{$scenario.feature.name}/#{$scenario.name}".gsub(' - ','').gsub(' ','-').gsub('(#','').gsub(')','')  # Gera url para o screenshot
-    #$scenario_path += '-' + Time.now.strftime('%d%m%Y%H%M%S').to_s
+    $scenario_path = "#{$scenario.feature.name}/#{$scenario.name}".gsub(' - ','').gsub(' ','-').gsub('(#','').gsub(')','')  # Gera url para o screenshot
+    $scenario_path += '-' + Time.now.strftime('%d%m%Y%H%M%S').to_s
 end
 
 Before('@carrinho') do |scenario|
@@ -29,27 +29,27 @@ Before('@carrinho') do |scenario|
   @produto = Produto.new(@store_id)
 end
 
-# Before('@appVersion') do |scenario|
-#   if ($feature != scenario.feature.name || $first) && CONFIG['verify_version']
-#     $first = false if $first == true
-#     $feature = scenario.feature.name
-#     puts $store_id + " | CatalogFront versão atual build: " + $appVersion.build_number[:catalogFront]
-#     puts $store_id + " | Checkout versão atual build: " + $appVersion.build_number[:checkout]
-#     puts $store_id + " | SServ versão atual build: " + $appVersion.build_number[:sserv]
-#     if $appVersion.build_number[:catalogFront] != $appVersion.get_catalog_front_version
-#       warn('Versão do catalogo mudou')
-#       puts "CatalogFront versão atual build: " + $appVersion.build_number[:catalogFront]
-#     end
-#     if $appVersion.build_number[:checkout] != $appVersion.get_checkout_version
-#       warn('Versão do checkout mudou')
-#       puts "Checkout versão atual build: " + $appVersion.build_number[:checkout]
-#     end
-#     if $appVersion.build_number[:sserv] != $appVersion.get_sserv_version
-#       warn('Versão do checkout mudou')
-#       puts "SServ versão atual build: " + $appVersion.build_number[:sserv]
-#     end
-#   end
-# end
+Before('@appVersion') do |scenario|
+  if ($feature != scenario.feature.name || $first) && CONFIG['verify_version']
+    $first = false if $first == true
+    $feature = scenario.feature.name
+    puts $store_id + " | CatalogFront versão atual build: " + $appVersion.build_number[:catalogFront]
+    puts $store_id + " | Checkout versão atual build: " + $appVersion.build_number[:checkout]
+    puts $store_id + " | SServ versão atual build: " + $appVersion.build_number[:sserv]
+    if $appVersion.build_number[:catalogFront] != $appVersion.get_catalog_front_version
+      warn('Versão do catalogo mudou')
+      puts "CatalogFront versão atual build: " + $appVersion.build_number[:catalogFront]
+    end
+    if $appVersion.build_number[:checkout] != $appVersion.get_checkout_version
+      warn('Versão do checkout mudou')
+      puts "Checkout versão atual build: " + $appVersion.build_number[:checkout]
+    end
+    if $appVersion.build_number[:sserv] != $appVersion.get_sserv_version
+      warn('Versão do checkout mudou')
+      puts "SServ versão atual build: " + $appVersion.build_number[:sserv]
+    end
+  end
+end
 
 AfterStep  do |step|
     contador = if $step_index == 2; 2; else; ($step_index / 2) + 1; end
@@ -62,14 +62,14 @@ AfterStep  do |step|
     $step_index += 2
     path_name = "reports/#{$store_id}/#{$scenario_path}/#{step_name}.png"
     page.save_screenshot(path_name)
-    #embed(path_name, 'image/png', 'SCREENSHOT')
+    embed(path_name, 'image/png', 'SCREENSHOT')
 end
 
 After do |scenario|
     if scenario.failed?
       path_name = "/reports/image.png"
       page.save_screenshot(path_name)
-      #embed(path_name, 'image/png', 'Screenshot')
+      embed(path_name, 'image/png', 'Screenshot')
     end
     MASS[@store_id] = @mass_bkp[@store_id]
     url_atual = (URI.parse(current_url)).to_s
